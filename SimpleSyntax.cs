68655d83cd5747e324469ae924747be45b4e0ae7
Luca Battesini <lucabattesini1@gmail.com>
2025-09-26 22:09:08 -0300

    Creating my first C# project

---
+++ SimpleSyntax.cs
@@ -0,0 +1,12 @@
+
+public class DeclaringVars
+{
+    string name = "Clara";
+    int age = 30;
+    double heigh = 1.68;
+    bool woman = true;
+
+    List<int> NumberList = new List<int> { 1, 2, 3 };
+
+    int[] NumberArray = { 1, 2, 3 };
+}
